54c72574249f9df892b338fb95b032970c33b27c
Sanjay Jyoti Dutta <sanjaydutta012@gmail.com>
2022-10-11 17:51:04 +0100

    push

--- Email_Slicer/Email_Slicer.py
+++ Email_Slicer/Email_Slicer.py
@@ -1,4 +1,5 @@
 # Email Slicer with Python
+# The strip() method removes any leading (spaces at the beginning) and trailing (spaces at the end) characters (space is the default leading character to remove)
 email = input("Enter Your Email: ").strip()
 username = email[:email.index("@")]
 domain_name = email[email.index("@")+1:]
