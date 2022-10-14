fdcac5a748f327f75be59ea3a433abe06adfaf30
Sanjay Jyoti Dutta <sanjaydutta012@gmail.com>
2022-10-14 12:48:51 +0100

    Create Print Coloured Text.py

---
+++ Print Coloured Text/Print Coloured Text.py
@@ -0,0 +1,5 @@
+import colorama
+from colorama import Fore, Back, Style
+colorama.init(autoreset= True)
+
+print(Fore.BLUE + Back.YELLOW + "Hi my name in Sanjay " + Fore.YELLOW + Back.BLUE + "I am learning Machine learning")
